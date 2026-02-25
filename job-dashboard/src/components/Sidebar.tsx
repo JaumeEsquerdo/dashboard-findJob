eb1850492ed0490b0f6346cd87b1fa7f428a92af
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-02-25 20:41:12 +0100

    ajustar con margin en las secciones interiores del Sidebar

--- job-dashboard/src/components/Sidebar.tsx
+++ job-dashboard/src/components/Sidebar.tsx
@@ -16,7 +16,7 @@ export const Sidebar = ({ sidebarOpen, setOpen }: sidebarProps) => {
             )}
             <aside
                 className={`
-        fixed top-0 right-0 h-full w-64 bg-white z-50 p-6 rounded-tl-2xl rounded-bl-2xl lg:rounded-2xl flex flex-col justify-start items-center gap-80 pt-30
+        fixed top-0 right-0 h-full w-64 bg-white z-50 p-6 rounded-tl-2xl rounded-bl-2xl lg:rounded-2xl flex flex-col justify-start items-center
         transform transition-transform duration-300
         ${sidebarOpen ? 'translate-x-0' : 'translate-x-full'}
         lg:static lg:translate-x-0 lg:h-auto
@@ -31,11 +31,11 @@ export const Sidebar = ({ sidebarOpen, setOpen }: sidebarProps) => {
                 </button>
 
                 {/* Contenido del sidebar */}
-                <header className="flex flex-col items-center gap-2">
+                <header className="flex flex-col items-center gap-2 mt-24">
                     <h3 className="font-medium text-xl">Detalles del trabajo</h3>
                     <p className="text-center">Clica algún trabajo para ver los detalles</p>
                 </header>
-                <section className="flex flex-col gap-2 items-center">
+                <section className="flex flex-col gap-2 items-center mt-auto mb-20">
                     <p>¿No sabes cómo empezar?</p>
                     <p>Te guiamos nosotros</p>
                 </section>
