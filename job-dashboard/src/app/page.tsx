e31e0185dabb3323d5b74cae454e9147d7eba6f2
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-02-26 11:43:48 +0100

    añadir filtros y render jobs

--- job-dashboard/src/app/page.tsx
+++ job-dashboard/src/app/page.tsx
@@ -1,12 +1,20 @@
 'use client'
 import Image from "next/image";
-import { useState } from "react";
-
+import { useState, useMemo } from "react";
 import { Sidebar } from "../components/Sidebar";
 import { Button } from "../components/Button";
 
+import dataJobs from '../data/jobs.json'
+
 export default function Home() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [jobs, setJobs] = useState(dataJobs.jobs)
+  const [filters, setFilters] = useState({
+    search: '',
+    experience: '',
+    location: '',
+  })
+  const uniqueLocations = [...new Set(jobs.map(job => job.location))]
 
   /* Evita error en SSR(servidor) (donde no hay window). En cliente, muestra onboarding si no existe "onboardingSeen" en localStorage. */
   const [isOnboardingVisible, setIsOnboardingVisible] = useState(() => {
@@ -18,6 +26,29 @@ export default function Home() {
     localStorage.setItem("onboardingSeen", "true")
     setIsOnboardingVisible(false)
   }
+
+  /* filtrado del search */
+  const filteredJobs = useMemo(() => {
+    return jobs.filter((job) => {
+
+      const matchesSearch =
+        job.title.toLowerCase().includes(filters.search.toLowerCase()) ||
+        job.company.toLowerCase().includes(filters.search.toLowerCase())
+
+
+      const matchesExperience =
+        filters.experience === '' ||
+        job.experience_level === filters.experience
+
+      const matchesLocation =
+        filters.location === '' ||
+        job.location === filters.location
+
+      return matchesSearch && matchesExperience && matchesLocation
+    })
+
+  }, [jobs, filters.experience, filters.location, filters.search])
+
 
   return (
     <div className="flex flex-1 flex-col h-full  lg:flex-row gap-6">
@@ -52,24 +83,84 @@ export default function Home() {
 
         {/* <div onClick={() => setSidebarOpen(!sidebarOpen)}>open sidebar</div>
         {sidebarOpen && <p>open</p>} */}
+        <div className="flex flex-col lg:flex-row">
+          <label className="relative block w-full lg:w-1/2 mt-8">
+            <input
+              value={filters.search}
+              onChange={(e) => setFilters({ ...filters, search: e.target.value })}
+              className="p-3 rounded-2xl w-full bg-whiteSpecial outline-2 outline-main focus:outline-main focus:shadow-[0_0_0_2px_var(--color-main),0_4px_0_2px_var(--color-main)] focus:outline-2 transition duration-100 " type="text" placeholder="Busca para consultar con precisión" />
+            <span className="absolute cursor-pointer bg-background rounded-2xl p-1.5 right-4 top-1/2 -translate-y-1/2 text-main">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+              >
+                <circle cx="11" cy="11" r="8" />
+                <line x1="21" y1="21" x2="16.65" y2="16.65" />
+              </svg>
+            </span>
+          </label>
+          <select value={filters.experience} onChange={(e) => setFilters({ ...filters, experience: e.target.value })}>
+            <option value="">Nivel</option>
+            <option value="Junior">Junior</option>
+            <option value="Mid-level">Mid-level</option>
+            <option value="Senior">Senior</option>
+          </select>
+          <select value={filters.location} onChange={(e) => setFilters({
+            ...filters, location: e.target.value
+          })}>
+            <option value="">Localización</option>
+            {uniqueLocations.map((loc) => (
+              <option key={loc} value={loc}>
+                {loc}
+              </option>
+            ))}
+          </select>
+        </div>
 
-        <label className="relative block w-full lg:w-1/2 mt-8">
-          <input
-            className="p-3 rounded-2xl w-full bg-whiteSpecial outline-2 outline-main focus:outline-main focus:shadow-[0_0_0_2px_var(--color-main),0_4px_0_2px_var(--color-main)] focus:outline-2 transition duration-100 " type="text" placeholder="Busca para consultar con precisión" />
-          <span className="absolute cursor-pointer bg-background rounded-2xl p-1.5 right-4 top-1/2 -translate-y-1/2 text-main">
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-            >
-              <circle cx="11" cy="11" r="8" />
-              <line x1="21" y1="21" x2="16.65" y2="16.65" />
-            </svg>
-          </span>
-        </label>
+        {/* tabla de render de jobs */}
+        <div className="overflow-x-auto">
+          <p>Hay {filteredJobs.length} resultados</p>
+          <table className="min-w-full bg-white runded-2xl shadow-md">
+            <thead className="bg-gray-50 text-left">
+              <tr>
+                <th className="p-4">Título</th>
+                <th className="p-4">Compañia</th>
+                <th className="p-4">Localización</th>
+                <th className="p-4">Nivel</th>
+                <th className="p-4">Remoto</th>
+                <th className="p-4">Salario</th>
+              </tr>
+            </thead>
 
+            <tbody>
+              {filteredJobs && filteredJobs.map((job) => (
+                <tr key={job.id} className="border-t">
+                  <td className="p-4 font-medium">{job.title}</td>
+                  <td className="p-4">{job.company}</td>
+                  <td className="p-4">{job.location}</td>
+                  <td className="p-4">{job.experience_level}</td>
+                  <td className="p-4">
+                    {job.remote ? 'Si' : 'No'}
+                  </td>
+                  <td className="p-4">
+                    {job.salary_min} - {job.salary_max} {job.currency}
+                  </td>
+                </tr>
+              ))}
+
+              {filteredJobs.length === 0 && (
+                <tr>
+                  <td className="p-6 text-center text-gray-500">
+                    No se han encontrado resultados
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </main>
 
       <Sidebar sidebarOpen={sidebarOpen} setOpen={setSidebarOpen} />
