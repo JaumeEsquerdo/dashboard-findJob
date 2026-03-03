a1fab3c451c8732c8240ae3c68652313ae93cfa9
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-03 14:27:46 +0100

    tipar Step y rehacer logica setStep

--- job-dashboard/src/context/HelperContext.tsx
+++ job-dashboard/src/context/HelperContext.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { createContext, useState, ReactNode } from 'react'
+type Step = 1 | 2 | 3 | null
+
 
 interface HelperContext {
     // isOpenHelper: boolean
     // setIsOpenHelper: React.Dispatch<SetStateAction<boolean>>
     // handleHelper: () => void
-    step: number | null
+    step: Step
     startGuide: () => void
     nextStep: () => void
     endGuide: () => void
@@ -15,19 +17,18 @@ interface HelperContext {
 export const HelperContext = createContext<HelperContext | undefined>(undefined)
 
 export const HelperProvider = ({ children }: { children: ReactNode }) => {
-    const [step, setStep] = useState<number | null>(null)
+    const [step, setStep] = useState<Step>(null)
 
     const startGuide = () => {
         setStep(1)
     }
 
     const nextStep = () => {
-
         setStep((prev) => {
-            if (prev === null) return null
-
-            const next = prev + 1
-            return next > 2 ? null : next
+            if (prev === 1) return 2
+            if (prev === 2) return 3
+            if (prev === 3) return null
+            return null
         })
     }
 
