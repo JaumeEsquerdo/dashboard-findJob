7e0d35389720c40242d99d1c7b0e6d35810ef871
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-10 17:49:29 +0100

    arreglar barra de scroll en la tabla

--- job-dashboard/src/components/RenderJobs.tsx
+++ job-dashboard/src/components/RenderJobs.tsx
@@ -124,84 +124,91 @@ export const RenderJobs = ({ filteredJobs, setSelectedJob, setSidebarOpen, selec
                         }}>Cerrar</Button>
                     </div>
                 )}
-                <div className="overflow-x-auto">
-                    <table className="min-w-full bg-white rounded-2xl">
-                        <thead className="bg-background text-left ">
-                            <tr >
-                                <th onClick={() => handleSort("title")} className="cursor-pointer p-4 rounded-tl-2xl whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Título     <span className="inline-block w-3 text-center">
-                                    {sortBy === "title" ? (sortDir === "asc" ? "▲" : "▼") : ""}
-                                </span>
-                                </th>
-                                <th onClick={() => handleSort("company")} className="cursor-pointer p-4 whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Compañia     <span className="inline-block w-3 text-center">
-                                    {sortBy === "company" ? (sortDir === "asc" ? "▲" : "▼") : ""}
-                                </span>
-                                </th>
-                                <th onClick={() => handleSort("location")} className="cursor-pointer p-4 whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Localización     <span className="inline-block w-3 text-center">
-                                    {sortBy === "location" ? (sortDir === "asc" ? "▲" : "▼") : ""}
-                                </span>
-                                </th>
-                                <th onClick={() => handleSort("experience_level")} className="cursor-pointer p-4 whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Nivel     <span className="inline-block w-3 text-center">
-                                    {sortBy === "experience_level" ? (sortDir === "asc" ? "▲" : "▼") : ""}
-                                </span>
-                                </th>
-                                <th onClick={() => handleSort("remote")} className="cursor-pointer p-4 whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Remoto     <span className="inline-block w-3 text-center">
-                                    {sortBy === "remote" ? (sortDir === "asc" ? "▲" : "▼") : ""}
-                                </span>
-                                </th>
-                                <th onClick={() => handleSort("salary_min")} className="cursor-pointer p-4 rounded-tr-2xl whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Salario     <span className="inline-block w-3 text-center">
-                                    {sortBy === "salary_min" ? (sortDir === "asc" ? "▲" : "▼") : ""}
-                                </span>
-                                </th>
-                            </tr>
-                        </thead>
 
-                        <tbody>
-                            {filteredJobs && sortedJobs.map((job, index) => (
-                                <tr key={`${job.id}-${index}`} onClick={() => {
-                                    setSelectedJob(job)
-                                    setSidebarOpen(true)
-                                }} className={`border-t border-gray-300 cursor-pointer transition duration-200 ${job === selectedJob ? 'bg-main text-whiteSpecial' : 'lg:hover:bg-fondoColor'}`}>
-                                    <td className="p-4 font-medium">{job.title}</td>
-                                    <td className="p-4">{job.company}</td>
-                                    <td className="p-4">{job.location}</td>
-                                    <td className="p-4">{job.experience_level}</td>
-                                    <td className="p-4">
-                                        {job.remote ? 'Si' : 'No'}
-                                    </td>
-                                    <td className="p-4">
-                                        {job.salary_min > 0 ? <>{job.salary_min} - {job.salary_max} {job.currency}</> : 'No indican salario'}
+                <div className="min-h-100 lg:min-h-80 flex flex-col">
+                    <div className="rounded-2xl overflow-hidden flex flex-col flex-1">
+                        <div className="overflow-x-auto flex-1">
+                            <table className="min-w-full bg-white">
+                                <thead className="bg-background text-left ">
+                                    <tr >
+                                        <th onClick={() => handleSort("title")} className="cursor-pointer p-4 rounded-tl-2xl whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Título     <span className="inline-block w-3 text-center">
+                                            {sortBy === "title" ? (sortDir === "asc" ? "▲" : "▼") : ""}
+                                        </span>
+                                        </th>
+                                        <th onClick={() => handleSort("company")} className="cursor-pointer p-4 whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Compañia     <span className="inline-block w-3 text-center">
+                                            {sortBy === "company" ? (sortDir === "asc" ? "▲" : "▼") : ""}
+                                        </span>
+                                        </th>
+                                        <th onClick={() => handleSort("location")} className="cursor-pointer p-4 whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Localización     <span className="inline-block w-3 text-center">
+                                            {sortBy === "location" ? (sortDir === "asc" ? "▲" : "▼") : ""}
+                                        </span>
+                                        </th>
+                                        <th onClick={() => handleSort("experience_level")} className="cursor-pointer p-4 whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Nivel     <span className="inline-block w-3 text-center">
+                                            {sortBy === "experience_level" ? (sortDir === "asc" ? "▲" : "▼") : ""}
+                                        </span>
+                                        </th>
+                                        <th onClick={() => handleSort("remote")} className="cursor-pointer p-4 whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Remoto     <span className="inline-block w-3 text-center">
+                                            {sortBy === "remote" ? (sortDir === "asc" ? "▲" : "▼") : ""}
+                                        </span>
+                                        </th>
+                                        <th onClick={() => handleSort("salary_min")} className="cursor-pointer p-4 rounded-tr-2xl whitespace-nowrap lg:hover:text-main lg:transition lg:duration-200">Salario     <span className="inline-block w-3 text-center">
+                                            {sortBy === "salary_min" ? (sortDir === "asc" ? "▲" : "▼") : ""}
+                                        </span>
+                                        </th>
+                                    </tr>
+                                </thead>
 
-                                    </td>
-                                </tr>
-                            ))}
+                                <tbody>
+                                    {filteredJobs && sortedJobs.map((job, index) => (
+                                        <tr key={`${job.id}-${index}`} onClick={() => {
+                                            setSelectedJob(job)
+                                            setSidebarOpen(true)
+                                        }} className={`border-t border-gray-300 cursor-pointer transition duration-200 ${job === selectedJob ? 'bg-main text-whiteSpecial' : 'lg:hover:bg-fondoColor'}`}>
+                                            <td className="p-2 lg:p-4 font-medium">{job.title}</td>
+                                            <td className="p-2 lg:p-4">{job.company}</td>
+                                            <td className="p-2 lg:p-4">{job.location}</td>
+                                            <td className="p-2 lg:p-4">{job.experience_level}</td>
+                                            <td className="p-2 lg:p-4">
+                                                {job.remote ? 'Si' : 'No'}
+                                            </td>
+                                            <td className="p-4">
+                                                {job.salary_min > 0 ? <>{job.salary_min} - {job.salary_max} {job.currency}</> : 'No indican salario'}
 
-                            {filteredJobs.length === 0 && (
-                                <tr>
-                                    <td className="p-6 font-medium text-gray-500">
-                                        No se han encontrado resultados
-                                    </td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
-                {filteredJobs.length > 0 && (
-                    <div ref={btnRef} className={`relative flex justify-center pb-28 my-8 ${step === 4 ? "pb-70" : ""}`}>
-                        {step === 4 && (
-                            <div className="absolute top-14 left-1/2 w-70 -translate-x-1/2 p-6 flex flex-col gap-2 bg-amber-50 sm:w-80 shadow-lg rounded-2xl lg:top-14 lg:gap-4 z-99">
-                                <p>Por último, si quieres ver más datos, al final de la lista de trabajos encontrarás
-                                    un botón para cargar más. Puedes seguir pulsándolo hasta ver todos los trabajos disponibles.</p>
-                                <Button className="w-full" bgColor="bg-amber-200" onClick={() => {
-                                    endGuide()
-                                    setActiveStep(0)
-                                }}>Cerrar</Button>
-                            </div>
-                        )}
-                        <Button onClick={loadJobs} disabled={loading || !hasMore}>
-                            {loading ? 'Cargando...' : hasMore ? 'Cargar más' : 'No hay más'}
-                        </Button>
+                                            </td>
+                                        </tr>
+                                    ))}
+
+                                    {filteredJobs.length === 0 && (
+                                        <tr>
+                                            <td className="p-4 lg:p-6 font-medium text-gray-500">
+                                                No se han encontrado resultados
+                                            </td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                )}
+                </div>
+                <div ref={btnRef} className={`relative flex justify-center pb-28 my-8 ${step === 4 ? "pb-70" : ""}`}>
+                    {filteredJobs.length > 0 && (
+                        <>
+                            {step === 4 && (
+                                <div className="absolute top-14 left-1/2 w-70 -translate-x-1/2 p-6 flex flex-col gap-2 bg-amber-50 sm:w-80 shadow-lg rounded-2xl lg:top-14 lg:gap-4 z-99">
+                                    <p>Por último, si quieres ver más datos, al final de la lista de trabajos encontrarás
+                                        un botón para cargar más. Puedes seguir pulsándolo hasta ver todos los trabajos disponibles.</p>
+                                    <Button className="w-full" bgColor="bg-amber-200" onClick={() => {
+                                        endGuide()
+                                        setActiveStep(0)
+                                    }}>Cerrar</Button>
+                                </div>
+                            )}
+                        </>
+                    )}
+                    <Button onClick={loadJobs} disabled={loading || !hasMore}>
+                        {loading ? 'Cargando...' : hasMore ? 'Cargar más' : 'No hay más'}
+                    </Button>
+                </div>
             </motion.div>
         </>
     );
