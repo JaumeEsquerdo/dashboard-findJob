62372d3f05fafa8d985ce1a47428d479d920551b
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-10 20:35:56 +0100

    ajustar responsive de los li de ajustes del navbar

--- job-dashboard/src/components/Navbar.tsx
+++ job-dashboard/src/components/Navbar.tsx
@@ -50,7 +50,7 @@ export const Navbar = () => {
                     )
                 })}
             </ul>
-            <ul className='flex gap-1 fixed bottom-6 right-4 bg-main p-2 z-20 rounded-xl shadow-lg lg:flex col lg:relative lg:shadow-none lg:right-auto lg:bottom-auto lg:gap-2'>
+            <ul className='flex gap-1 fixed bottom-6 right-4 bg-main p-2 z-20 rounded-xl shadow-lg lg:flex-col lg:relative lg:shadow-none lg:right-auto lg:bottom-auto lg:gap-2'>
                 <li onClick={step === null ? startGuide : undefined} className={`relative`}>
                     {step === 1 && (
                         <div className='absolute -top-60 -left-52 w-70 sm:-left-60 p-6 flex flex-col gap-2 bg-amber-50 sm:w-80 shadow-lg rounded-2xl lg:left-0 lg:gap-4'>
